50c1cf17aa52abc82d19d9615e08f890bc123204
Sylvain Saubier <mail@sylsau.com>
2018-04-26 19:56:20 +0200

    better gen script

--- make.sh
+++ make.sh
@@ -1,4 +1,12 @@
 #!/bin/bash -
+set -e -E
 
-echo -e "\n\n---\n\n" | cat ./c_coding_guidelines.md - ./project_hierarchy_standard.md > README.md  &&\
-	pandoc ./README.md -V geometry="margin=5pc" -s -o c_coding_guidelines.pdf
+OUT_PDF="./c_coding_guidelines.pdf"
+BREAK="/tmp/brk"
+trap 'rm -v $BREAK' EXIT
+
+echo -e "\n\n---\n\n" > "$BREAK"
+cat ./syntax.md "$BREAK" ./file_organization.md "$BREAK" ./references.md > README.md
+echo "\\maketitle \\vfill \\tableofcontents \\vfill  \\newpage" | \
+    pandoc - ./README.md -V title="C Coding Guidelines" -V geometry="margin=5pc" -s -o "$OUT_PDF"
+[[ $? -eq 0 ]] && [[ $1 ]] && xdg-open "$OUT_PDF" &
